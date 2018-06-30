d5422728b9d3a2f66c7231f5a44050f96bd35e70
Alicja Dutkiewicz <ala@dutkiewicz.waw.pl>
2018-06-30 15:56:01 +0200

    graphics changed

--- evolver.py
+++ evolver.py
@@ -1,20 +1,24 @@
 from shapely.geometry import Polygon,MultiPolygon
 import matplotlib.pyplot as plt
+from numpy.random import choice
+
+colours=['brown','lightblue','khaki','gold','lavender','tan','wheat','white','darkgray','beige','ivory']
 
 #first_layer = list of points in a start layer[(x1,y1),..]
 #N_apexes = number of apexes of polygons aproximating layers; by default same as number of poinst in the first_layer
 #min_area: the evolution stops when the area inside the layer is smaller than min_area
-
-def evolve_agat(first_layer, custom_plot, N_apexes=-1,layer_width=0.05,min_area=0.001):
-    evolve_agat_on(first_layer=first_layer, custom_plot=plt, N_apexes=N_apexes, layer_width=layer_width, min_area=min_area)
-
-def evolve_agat_on(first_layer, custom_plot, N_apexes=-1,layer_width=0.05,min_area=0.001):
+def evolve_agat(first_layer, custom_plot=plt, N_apexes=-1,layer_width=0.05,min_area=0.001):
     if(N_apexes==-1): N_apexes=len(first_layer)
+    fig = plt.figure(1, figsize=(5,5), dpi=90)
+    ax = fig.add_subplot(111)
+    ax.set_aspect(1)
+    ax.set_title('Agate')
     layer=MultiPolygon([Polygon(first_layer)])
     while(layer.area>min_area):
         for polygon in layer:
             x,y=polygon.exterior.xy
-            custom_plot.plot(x,y)
+            ax.plot(x,y,choice(colours,1)[0])
+            ax.fill(x,y,choice(colours,1)[0])
         layer=layer.buffer(-layer_width,N_apexes)
         if(layer.__class__.__name__=='Polygon'):
             layer=MultiPolygon([layer])
