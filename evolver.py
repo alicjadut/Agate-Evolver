8e173763e9f912727f4a52a4f1d99e1abcfe4bb2
Alicja Dutkiewicz <ala@dutkiewicz.waw.pl>
2018-06-30 13:39:15 +0200

    comments added

--- evolver.py
+++ evolver.py
@@ -1,16 +1,9 @@
-
-# coding: utf-8
-
-# In[5]:
-
-
 from shapely.geometry import Polygon
 import matplotlib.pyplot as plt
 
-
-# In[42]:
-
-
+#first_layer = list of points in a start layer[(x1,y1),..]
+#N_apexes = number of apexes of polygons aproximating layers; by default same as number of poinst in the first_layer
+#min_area: the evolution stops when the area inside the layer is smaller than min_area
 def evolve_agat(first_layer, N_apexes=-1,layer_width=0.05,min_area=0.001):
     if(N_apexes==-1): N_apexes=len(first_layer)
     layer=Polygon(first_layer)
