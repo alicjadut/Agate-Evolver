5c478e23856316ddb917857d4b6b142ec9ef9e7c
Wojciech Ciszewski <wojtrz13@gmail.com>
2018-06-30 17:02:48 +0200

    Drawing changes

--- draw.py
+++ draw.py
@@ -15,14 +15,16 @@ cid = None
 def agate_init(axis):
     axis.cla()
     axis.autoscale(enable=False)
+    axis.set_aspect(1)
+    axis.get_xaxis().set_visible(False)
+    axis.get_yaxis().set_visible(False)
     global marked_points
     marked_points = []
 
 root = Tk.Tk()
 root.wm_title("Agate evolver")
 
-
-f = Figure(figsize=(5, 5), dpi=90)
+f = Figure(figsize=(15, 15), dpi=90)
 a = f.add_subplot(111)
 
 agate_init(a)
@@ -33,10 +35,11 @@ canvas.draw()
 canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
 def on_drag(event):
-    a = event.canvas.figure.axes[0]
-    a.plot(event.xdata, event.ydata, 'ro')
-    marked_points.append((event.xdata, event.ydata))
-    event.canvas.draw()
+    if event.xdata is not None and event.ydata is not None:
+        a = event.canvas.figure.axes[0]
+        a.plot(event.xdata, event.ydata, 'ro')
+        marked_points.append((event.xdata, event.ydata))
+        event.canvas.draw()
 
 def on_press(event):
     if event.button == 1:
@@ -47,8 +50,9 @@ def on_press(event):
 def on_release(event):
     if event.button == 1:
         canvas.mpl_disconnect(cid)
-        evolve_agat(marked_points, fig=event.canvas.figure)
-        event.canvas.draw()
+        if len(marked_points) >= 3:
+            evolve_agat(marked_points, fig=event.canvas.figure, layer_width=0.005)
+            event.canvas.draw()
 
 canvas.mpl_connect('button_press_event', on_press)
 canvas.mpl_connect('button_release_event', on_release)
