8134974db615a2134f83ce88c09c688af8f548fb
Alicja Dutkiewicz <ala@dutkiewicz.waw.pl>
2018-06-30 14:44:28 +0200

    minor changes to handle separation

--- evolver.py
+++ evolver.py
@@ -1,14 +1,16 @@
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon,MultiPolygon
 import matplotlib.pyplot as plt
 
 #first_layer = list of points in a start layer[(x1,y1),..]
 #N_apexes = number of apexes of polygons aproximating layers; by default same as number of poinst in the first_layer
 #min_area: the evolution stops when the area inside the layer is smaller than min_area
+
 def evolve_agat(first_layer, N_apexes=-1,layer_width=0.05,min_area=0.001):
     if(N_apexes==-1): N_apexes=len(first_layer)
-    layer=Polygon(first_layer)
+    layer=MultiPolygon([Polygon(first_layer)])
     while(layer.area>min_area):
-        x,y=layer.exterior.xy
-        plt.plot(x,y)
+        for polygon in layer:
+            x,y=polygon.exterior.xy
+            plt.plot(x,y)
         layer=layer.buffer(-layer_width,N_apexes)
 
