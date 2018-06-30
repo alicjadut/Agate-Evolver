6095fbce231bc1ed07aa90ceaab15cbb6a71fc87
skurdzialek <s.kurdzialek@student.uw.edu.pl>
2018-06-30 15:15:38 +0200

    parametry domyslne

--- sincircle.py
+++ sincircle.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 # Generating circle modulated by sin
 
 #Curve parameters
-r_max=0.9 # maximal distance from (0,0) point to the curve (between 0 and 1)
+r_max=1 # maximal distance from (0,0) point to the curve (between 0 and 1)
 modulation=0.05 # amplitude of sin to the average radius of the circle ratio (between 0 and 1)
-period= (2*np.pi)/200 # period of sin in radians
+period= (2*np.pi)/50 # period of sin in radians
 
 number_of_points=10000 # how many points are used in curve approximation
 
 # target curve in polar coordinates
 
-def sin_circle(r_max, modulation, period, fi):
+def sin_circle(fi, r_max=r_max, modulation=modulation, period=period):
   
     #returns r for the specific angle (fi)
     #r_max, modulation, period- parameters of the curve
@@ -22,18 +22,18 @@ def sin_circle(r_max, modulation, period, fi):
     freq = (2*np.pi)/period
     
     return r_0 * (1 + a*np.sin(freq*fi))
-    
+
+
 def polar_curve(fi):
-    return sin_circle(r_max, modulation, period, fi)
+    return sin_circle(fi, r_max, modulation, period)
   
-def make_xy_list( polar_curve, number_of_points):
+def make_xy_list( function=polar_curve, number_of_points=number_of_points):
     points_xy=[]
     for fi in np.linspace(0, 2*np.pi, number_of_points):
-        x , y = polar_curve(fi)*np.cos(fi), polar_curve(fi)*np.sin(fi)
+        x , y = function(fi)*np.cos(fi), function(fi)*np.sin(fi)
         points_xy.append((x,y))
         
     return points_xy
     
 
-points_xy = make_xy_list(polar_curve, number_of_points)   
-    
+points_xy = make_xy_list(polar_curve, number_of_points)
